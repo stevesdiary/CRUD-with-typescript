22458b76a557263ddb734d461104fc642ab6864c
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-07 21:15:29 +0100

    Update EmployeeController.ts

--- src/controllers/EmployeeController.ts
+++ src/controllers/EmployeeController.ts
@@ -14,9 +14,13 @@ class EmployeeController{
       })
     } catch (error) {
       console.log(error);
-      // return next(error)
+      return response.status(400).send({
+        message: "Unable to get record due to error",
+        error: error
+      })
     }
   }
+
   createEmployee = async (request: Request, response: Response, next: NextFunction) => {
     try {
       let {name, email, phone, DOB, DOJ } = request.body;
@@ -24,7 +28,6 @@ class EmployeeController{
          name, email, phone, DOB, DOJ
       });
       await employee.save();
-      console.log(employee);
 
       return response.status(201).send({
         statusCode: 201,
@@ -35,7 +38,7 @@ class EmployeeController{
       console.log(error);
       return response.status(400).send({
         status: "fail",
-        // mesage: error.mesage,
+        mesage: 'An error occoured',
         error: error
       })
     }
@@ -43,9 +46,13 @@ class EmployeeController{
 
   getEmployee = async (request: Request, response: Response) => {
     try {
-      const { id } = request.params;
-      const employee = await EmployeeModel.findById({$where:{id:id}});
-      console.log(employee);
+      const id = request.params.id;
+      const employee = await EmployeeModel.findById({_id: id});
+      if(!employee){
+        return response.status(404).send({
+          message: "Record not found",
+        })
+      }
       
       return response.status(200).send({
         statusCode: 200,
@@ -54,35 +61,57 @@ class EmployeeController{
       })
     } catch (error) {
       console.log(error);
-      // return next(error)
+      return response.status(400).send({
+        message: "Unable to get record due to error",
+        error: error
+      })
     }
   }
 
-  // updateEmployee = async (request: Request, response: Response, next: NextFunction) => {
-  //   try {
-  //     const { id } = request.params;
-  //     const {name, email, phone, DOB, DOJ } = request.body;
-  //     const employee = EmployeeModel.findById(id)
-  //     if (employee){
-  //       employee.name = name;
-  //       employee.email = email;
-  //       employee.phone = phone;
-  //       employee.DOB = DOB;
-  //       employee.DOJ = DOJ;
+  updateEmployee = async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { id } = request.params;
+      const { name, email, phone, DOB, DOJ } = request.body;
+      const employee = await EmployeeModel.findByIdAndUpdate(id, {
+        $set: {
+          name: name,
+          email: email,
+          phone: phone,
+          DOB: DOB,
+          DOJ: DOJ
+        },
+        $currentDate: { updatedAt: true }
+      }, { new: true, select: '_id name email phone DOB DOJ' });
+  
+      if (!employee) {
+        return response.status(404).send({
+          message: "Employee not found",
+        });
+      }
+      return response.status(200).send({
+        statusCode: 200,
+        message: "Employee updated successfully",
+        result: employee
+      })
+    } catch (error) {
+      console.log(error);
+      return response.status(400).send({
+        message: "Unable to update record due to error",
+        error: error
+      })
+    }
+  }
 
-  //       await employee.set();
-  //     };
-      
-  //     return response.status(200).send({
-  //       statusCode: 201,
-  //       message: "Employee updated successfully",
-  //       result: employee
-  //     })
-  //   } catch (error) {
-  //     console.log(error);
-  //     // return next(error)
-  //   }
-  // }
+  deleteEmployee = async (request: Request, response: Response ) => {
+    try {
+      const id = request.params;
+      const record = await EmployeeModel.findByIdAndDelete({id});
+      return response.status(200).send({message: "Record deleted successfully"})
+    } catch (error) {
+      console.log(error);
+      return response.status(400).send({message: "Unable to delete record due to error", error: error})
+    }
+  }
 }
 
 export default new EmployeeController();
