7f8bf7b6b5084d4993e97c1fb319299abd671aac
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-08 10:26:29 +0100

    Update employee.ts

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import EmployeeController from '../controllers/employeeController';
+import EmployeeController from '../controllers/EmployeeController';
 const router = express.Router();
 
 router.get('/', EmployeeController.getAllEmployee);
+
 router.post('/employee', EmployeeController.createEmployee);
+
 router.get('/employee/:id', EmployeeController.getEmployee);
 
 router.put('/:id', EmployeeController.updateEmployee );
 
 router.delete('/:id', EmployeeController.deleteEmployee);
+
 export default router;
