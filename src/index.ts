c67fd5a5b52a7ecef66134e2c9f0babed80ce7bb
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-15 11:25:02 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,11 @@
 require('dotenv').config();
 import express from "express";
-import mongoose from "mongoose";
+import { connectDb } from "./db/dbConnection";
 import router from './routes/employee';
 const app = express();
 app.use(express.json());
 
-if (!process.env.DB_URL) {
-  console.error('MONGODB_URI environment variable is not defined.');
-  process.exit(1);
-}
-const mongo_url: string = process.env.DB_URL
-mongoose.connect(mongo_url, {
-  dbName: 'Cluster0',
-})
-  .then(() => {
-    console.log("Database connected!");
-  })
-  .catch((error) => console.log(error)
-);
-
+connectDb();
 app.use('/', router);
 
 app.listen(4000, () => {
