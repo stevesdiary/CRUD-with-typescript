c762e851a200cb105d72130187b780fa1fc7dc0d
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-20 16:22:34 +0100

    Update employee.ts

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -2,6 +2,12 @@ import express from 'express';
 import EmployeeController from '../controllers/EmployeeController';
 const router = express.Router();
 
+import registerController from "../controllers/registerController";
+
+router.post("/register", registerController.register);
+
+router.post("/login", registerController.loginUser);
+
 router.get('/', EmployeeController.getAllEmployee);
 
 router.post('/employee', EmployeeController.createEmployee);
