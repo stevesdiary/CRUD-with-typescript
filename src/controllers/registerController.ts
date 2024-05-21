d2c41810cb0e5f469a518dccd28662a4e25ca87e
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-05-21 10:06:29 +0100

    Update registerController.ts

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,26 +1,51 @@
 import express, { Request, Response, NextFunction } from "express";
-import { register as registerUser, login as authenticateUser } from "../service/authenticateUser";
+import jwt from "jsonwebtoken";
+import { UserModel } from "../models/user";
+import bcrypt from 'bcrypt';
 
-interface AuthResult {
-  user: UserDocument;
-  token: string;
-}
-
-const register = async (req: Request, res: Response, next: NextFunction) => {
-  const { name, email, password, type } = req.body;
+const register = async (request: Request, response: Response, next: NextFunction) => {
+  const { name, email, password, type } = request.body;
   try {
-    const { user, token } = await registerUser(name, email, password, type);
-    res.status(201).json({ user, token });
+    if (!email || ! password || !type || !name) {
+      return response.send("Email, Name, password and type are required");
+    }
+    const userExists = await UserModel.findOne({email});
+    if(userExists){
+      return response.status(400).send({message: `Employee record for ${email} already exists, login with your email and password`})
+    }
+    const encryptedPassword  = await bcrypt.hash(password, 10)
+    const user = await UserModel.create({ name, email: email.toLowerCase(), password: encryptedPassword, type});
+    await user.save();
+    response.status(201).send({message: `User ${name} registered successfully`, user});
   } catch (error) {
+    console.log(error);
     return next(error);
   }
 };
 
-const loginUser = async (request: Request, response: Response, next: NextFunction): Promise<AuthResult> => {
+const loginUser = async (request: Request, response: Response, next: NextFunction) => {
   const { email, password } = request.body;
   try {
-    const { user, token } = await authenticateUser( email, password );
-    return response.status(200).send({ user, token });
+    const secret = process.env.SECRETKEY || 'secretstring';
+    console.log(secret)
+    if (!email || !password ) {
+      return response.send("Login with registered email and password");
+    }
+    const foundUser = await UserModel.findOne({ email });
+    console.log(foundUser);
+    if (!foundUser){
+      return response.send("User email is not correct");
+    }
+    const isMatch = bcrypt.compareSync(password, foundUser.password);
+    if (isMatch){
+      const token = jwt.sign({ email: foundUser.email, password: foundUser.type }, secret, { expiresIn: '2 days', });
+      let userWithoutPassword = await UserModel.findOne({ email}).select("-password");
+    
+      return response.status(200).send({ message: 'Logged in successfully', user: userWithoutPassword, token });
+    }
+    else {
+      return response.status(403).send("Password is not correct");
+    }
   } catch (error) {
     return next(error);
   }
