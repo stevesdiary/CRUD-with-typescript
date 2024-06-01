fb95cd3a738281691f6ce84c1882838cdc92bd4a
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-06-01 02:29:42 +0100

    Update loginController.ts

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -8,21 +8,20 @@ const loginUser = async (request: Request, response: Response, next: NextFunctio
   const { email, password } = request.body;
   try {
     const secret = process.env.SECRETKEY || 'secretstring';
-    console.log(secret)
+    // console.log(secret)
     if (!email || !password ) {
       return response.send("Login with registered email and password");
     }
     const foundUser = await UserModel.findOne({ email });
-    console.log(foundUser);
     if (!foundUser){
       return response.send("User email is not found");
     }
     const isMatch = bcrypt.compareSync(password, foundUser.password);
     if (isMatch){
       const token = jwt.sign({ email: foundUser.email, type: foundUser.type }, secret, { expiresIn: '2 days', });
-      let userWithoutPassword = await UserModel.findOne({ email}).select("-password");
+      let user = await UserModel.findOne({ email }).select("-password");
       // delete foundUser['password'] = {foundUser.password};
-      return response.status(200).send({ message: `Logged in successfully as ${foundUser.type}`, userWithoutPassword, token });
+      return response.status(200).send({ message: `Logged in successfully as ${foundUser.type}`, user, token });
     }
     else {
       return response.status(403).send("Password is not correct");
