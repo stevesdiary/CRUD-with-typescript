da3ea67c439ced8df619f133a719fcb4f6f2b0a5
Stephen Oyeyemi <stephenoyeyemi@gmail.com>
2024-06-01 02:29:26 +0100

    Update authentication.ts

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -1,34 +1,47 @@
 require('dotenv').config();
 import express, { Request, Response, NextFunction } from "express";
-import jwt, {Secret} from 'jsonwebtoken';
-import { request } from 'express';
+import { string } from "joi";
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-// interface CustomHeaders extends Headers {
-//   [key: string]: string | string[];
-// }
+const secret:string = process.env.SECRETKEY || "SECRETKEY";
 
-const authentication = async (request: Request, response: Response, next: NextFunction) => {
+export const authentication = async (request: Request, response: Response, next: NextFunction) => {
   try {
-    const { email, password } = request.body;
-    if (!email || !password) {
-      return response.status(400).send('Email and password are required')
-    }
-    const authHeader = (request.headers as any).get('authorization');
-    const secret = process.env.SECRETKEY;
-    const emailFromHeaders = request.headers
+    const header = request.headers['authorization'];
     let token = request.headers.authorization;
-    if (!token) {
-      console.log("No valid token here");
-      return response.status(401).send("No valid token here") 
-    }
-    token = authHeader?.split(' ')[1];
+    token = header?.split(' ')[1];
     if (token == null || token.length === 0) {
       console.log("Unauthorised or wrong token");
-      return response.send({ message: "No valid token here"});
-    } 
+      return response.send({ message: "Invalid or expired token" });
+    }
+    next();
   } catch (error) {
     console.log(error);
+    return response.status(500).send({ message: "Oops error occoured", error: error });
   }
 }
 
-export default authentication;
+export const verifyUser = (...allowedType: any[])  => {
+  return (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const header = request.headers['authorization'];
+      let token: string = request.headers.authorization || '';
+      token = header?.split(' ')[1] || 'STRING';
+      const decoded = jwt.verify(token, secret)
+      
+      let userType = (decoded as JwtPayload).type;
+      // console.log("User", userType);
+      if (allowedType.includes(userType)) {
+        next();
+      } else {
+        console.log("Access denied to this route for users");
+        response.status(401).send({
+          statusCode: 401,
+          message: "You are NOT authorised to access this route!",
+        });
+      }
+    } catch (error) {
+      response.status(400).send({message:"Error occoured", error: error});
+    }
+  }
+};
